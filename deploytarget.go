6e570841149088f778bf6062fbfafff386fc25c1
ben.joyce@gmail.com <3Bpa346iu17Ph>
2017-07-16 19:01:55 +0100

    Added HTTP response header (application/json). Added any servers that match the query to an array of servers to be returned, with the Hostname and Application data included. Return the result as JSON.

--- deploytarget.go
+++ deploytarget.go
@@ -30,44 +30,45 @@ type Environment struct {
 
 func main() {
 
-	file, e := ioutil.ReadFile("./moreservers.json")
-	if e != nil {
-		fmt.Printf("File error: %v\n", e)
-		os.Exit(1)
-	}
-	fmt.Printf("%s\n", string(file))
-
-	var servers ServerList
-	json.Unmarshal(file, &servers)
-	fmt.Printf("Results: %v\n", servers)
-
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		var env = r.URL.Query().Get("env")
 		var app = r.URL.Query().Get("app")
-		//cmd := exec.Command("cmd", "/C", "c:/go/bin/jq-win64.exe", "-r", "[.[] | select(.environments[] | contains(\""+env+"\")) | select(.applications[] | contains(\""+app+"\") ) | .] | unique", "servers.json")
-		//var out bytes.Buffer
-		//var stderr bytes.Buffer
-		//cmd.Stdout = &out
-		//cmd.Stderr = &stderr
-		//err := cmd.Run()
-		//if err != nil {
-		//		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-		//		return
-		//	}
+
+		fmt.Printf("env="+env+"\n")
+		fmt.Printf("app="+app+"\n")
+
+		file, e := ioutil.ReadFile("./servers.json")
+		if e != nil {
+			fmt.Printf("File error: %v\n", e)
+			os.Exit(1)
+		}
+
+		var servers ServerList
+		json.Unmarshal(file, &servers)
+
+		serverResult := make([]Server,0)
 
 		for i := range servers.Servers {
+			//fmt.Printf("%s\n",servers.Servers[i].Hostname)
 			for j := range servers.Servers[i].Applications {
+				//fmt.Printf("-%s\n",servers.Servers[i].Applications[j].Id)
 				for k := range servers.Servers[i].Applications[j].Environments {
+					//fmt.Printf("--%s\n",servers.Servers[i].Applications[j].Environments[k].Id)
 					if servers.Servers[i].Applications[j].Id == app {
 						if servers.Servers[i].Applications[j].Environments[k].Id == env {
-							fmt.Fprintf(w, servers.Servers[i].Hostname)
+							//fmt.Printf("Target:%s\n",servers.Servers[i].Applications[j].Environments[k].Id)
+							applications := make([]Application,0)
+							applications = append(applications,servers.Servers[i].Applications[j])
+							newServer := Server{Hostname:servers.Servers[i].Hostname,Applications:applications}
+							serverResult = append(serverResult,newServer)
 						}
 					}
 				}
 			}
 		}
 
-		//fmt.Fprintf(w, out.String())
+		json.NewEncoder(w).Encode(serverResult)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
